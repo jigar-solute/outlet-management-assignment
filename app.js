b480714e0e046062b2a6dda20b6e69a762ea6396
VishalP-SoluteLabs <vishal.p@solutelabs.com>
2023-04-13 15:16:02 +0530

    vishal

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+
+const app = express();
+
+
+app.listen(process.env.PORT || 3000, () => {
+
+    console.log(`Server started at port: ${process.env.PORT || 3000}`)
+})
