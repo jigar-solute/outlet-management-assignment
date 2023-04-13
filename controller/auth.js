6dab392cb5932f819488466d15f7861febdbcc42
VishalP-SoluteLabs <vishal.p@solutelabs.com>
2023-04-13 17:54:38 +0530

    login added

--- controller/auth.js
+++ controller/auth.js
@@ -9,16 +9,17 @@ exports.signup = async (req, res, next) => {
     const password = req.body.password;
     const userRole = req.body.userRole;
 
-
     try {
-        const hashedPwd = await bcrypt.hash(password, 12)
+        const hashedPwd = await bcrypt.hash(password, 12);
     
         const user = new User({
           email: email,
           password: hashedPwd,
           userRole: userRole
         });
+
         const result = await user.save();
+        
         res.status(201).json({
           message: 'User Created Successfully..!!',
           userId: result._id
@@ -26,4 +27,39 @@ exports.signup = async (req, res, next) => {
       } catch (err){
         console.log(err)
       }
+}
+
+
+
+exports.login = async (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    try {
+        const user = await User.findOne({
+            email: email
+          })
+        if (!user) {
+            const error = new Error('A user with this email could not be found.');
+            error.statusCode = 401;
+            throw error;
+          }
+        
+         const isEqual = await bcrypt.compare(password, user.password);
+        if (!isEqual) {
+         const error = new Error('Wrong password!');
+         error.statusCode = 401;
+         throw error;
+       }  
+
+    
+        res.status(200).json({
+         message: 'User Found',  
+         userId: user._id.toString(),
+         userRole: user.userRole
+        });
+
+    } catch (err) {
+        console.log(err)
+    }
 }
--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,9 @@ const router = express.Router();
 const authController = require('../controller/auth.js')
 
 
-router.put('/signup', authController.signup)
+router.put('/signup', authController.signup);
+
+router.post('/login', authController.login);
+
 
 module.exports = router;
