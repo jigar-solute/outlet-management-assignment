cc6713bf6770ee3cdf7402bfa9cdc56727bf1d98
VishalP-SoluteLabs <vishal.p@solutelabs.com>
2023-04-13 17:12:47 +0530

    created signup function

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 require('dotenv').config(); //to load environment file
 
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const mongoose  = require('mongoose');
+
+const adminRoutes = require('./routes/admin.js');
+const authRoutes = require('./routes/auth.js');
 
 const app = express();
-
 
 
 app.use(bodyParser.json()); // application/json           //to parse json data from incoming requests
 
 
+app.use('/admin', adminRoutes);
+app.use('/auth', authRoutes);
 
-app.listen(process.env.PORT || 3000, () => {
 
-    console.log(`Server started at port: ${process.env.PORT || 3000}`)
+mongoose.connect('mongodb+srv://admin-vishal:OiXu0VqZrnApu31L@cluster0.tqqbw.mongodb.net/outlet-management?w=majority')
+.then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+
+        console.log(`Server started at port: ${process.env.PORT || 3000}`)
+    })
 })
+.catch(err => console.log(err))
--- controller/auth.js
+++ controller/auth.js
@@ -1,6 +1,27 @@
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user.js');
 
 
 exports.signup = async (req, res, next) => {
 
+    const email = req.body.email;
+    const password = req.body.password;
 
+
+    try {
+        const hashedPwd = await bcrypt.hash(password, 12)
+    
+        const user = new User({
+          email: email,
+          password: hashedPwd
+        });
+        const result = await user.save();
+        res.status(201).json({
+          message: 'User Created Successfully..!!',
+          userId: result._id
+        });
+      } catch (err){
+        console.log(err)
+      }
 }
