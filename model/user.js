6c96275f906e6e76df6356b41fafa8fd0a83998c
jigar-solute <jigar.p1@solutelabs.com>
2023-04-14 20:02:43 +0530

    user model

--- model/user.js
+++ model/user.js
@@ -10,12 +10,13 @@ const userSchema = new Schema({
     type: String,
     required: true
   },
+
   userRole: {
     type: String,
     enum: {
       values: ["admin", "outlet-manager","area-manager"],  //user can choose from this only
       message: `{VALUE} is not valid, use from ["admin", "outlet-manager","area-manager"] this only` //{VALUE} it means the value that the user provides
-    }  //message will show error if the value doen't match from the above array
+    }  //message will show error if the value doen't match from the above array..
   },
   products: [
     {
