9e834c8d28c232ccf0f318a6c9f28cf00ace52e6
jigar-solute <jigar.p1@solutelabs.com>
2023-04-14 19:48:10 +0530

    main.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const monogoose = require('mongoose');
-const outlateRouter = require('./routes/outletmanager');
+const authRoutes = require('./routes/authroutes');
+const outlateroutes = require('./routes/outletroutes');
 const app = express();
 const CONNECTION = "mongodb+srv://admin-vishal:OiXu0VqZrnApu31L@cluster0.tqqbw.mongodb.net/outlet-management?w=majority"
 
-app.use('/auth',outlateRouter)
+app.use('/auth',authRoutes)
+app.use('/outlate',outlateroutes)
 monogoose.connect(CONNECTION).then
     (() => {
         console.log("connected to database")
