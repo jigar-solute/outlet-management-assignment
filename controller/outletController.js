5b45ab3b13418be80bfbccc09469906df7a0f16a
jigar-solute <jigar.p1@solutelabs.com>
2023-04-14 19:49:20 +0530

    outletcontrollermodel

--- controller/outletController.js
+++ controller/outletController.js
@@ -1,48 +1,79 @@
 
 const bcrypt = require('bcrypt')
-const path=require('path')
+const path = require('path')
+const express = require('express');
+const app = express();
 const jwt = require('jsonwebtoken')
-
-const outletmodel = require('../model/outletmodel')
+const outlateRouter = require('../routes/authroutes');
+const User = require('../model/user')
+const Outlet = require('../model/outletstoremodel')
 
 const signup = async (req, res) => {
-     
+
     const { email, password, userRole } = req.body
     const hashpassword = await bcrypt.hash(password, 16)
-    const extinguser = await outletmodel.findOne({ email: email })
+    const extinguser = await User.findOne({ email: email })
     if (extinguser) {
-      return res.status(400).send("user already exist")
+        return res.status(400).send("user already exist")
     }
-    const user =  new outletmodel({ email:email,password:hashpassword,userRole:userRole })
-    try {
-
-       await user.save()
-       res.status(200).json(user)
-    }
-    catch (err) {
-      //error handling middleware
-      console.log(err)
-    }
-  }
-  const signin = async (req, res) => {
+    const user = new User(
+        {
+            email: email,
+            password: hashpassword,
+            userRole: userRole
+        })
+      try {
+        await user.save()
+        res.status(200).json(user)
+        }
+         catch (err) {
+        console.log(err)
+                }
+}
+const signin = async (req, res) => {
     try {
         const { email, password } = req.body
-        const exitinguser = await outletmodel.findOne({ email: email })
+        const exitinguser = await User.findOne({ email: email })
         if (!exitinguser) {
-          res.status(400).json("user not found")
+            res.status(400).json("user not found")
         }
         const crditinal = await bcrypt.compare(password, exitinguser.password)
         if (!crditinal) {
-          res.status(400).json("invalid password")
+            res.status(400).json("invalid password")
         }
-        else{
-            res.send("login successfull")
-        }
-    
-      }
-      catch (err) {
-        res.status(402).json("Some Problem occure")
-      }
+        else if (exitinguser.userRole === "outlet-manager") {
 
-  }
-  module.exports = { signup,signin }
+            res.send(" outlate login successfull")
+
+        }
+    }
+    catch (err) {
+        res.status(402).json("Some Problem occure")
+    }
+
+}
+const addoutlateStore = async (req, res) => {
+    const outlet = new Outlet({
+        name: req.body.name,
+        city: req.body.city,
+        state: req.body.state,
+        address: req.body.address,
+        status: req.body.status,
+        timing: req.body.timing,
+        manager: "643909ced0c6e2af78842fcb",
+    })
+    try {
+        await outlet.save()
+        res.status(200).json(outlet)
+
+    }
+    catch (err) {
+        console.log(err)
+    }
+
+
+}
+const addtocart=async(req,res)=>{
+    
+}
+module.exports = { signup, signin, addoutlateStore,addtocart }
