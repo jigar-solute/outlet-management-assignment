9aa75a8b374adb74d1a3529e582d5f5f136ca650
jigar-solute <jigar.p1@solutelabs.com>
2023-04-14 10:14:26 +0530

    outletmanager routes

---
+++ routes/outletmanager.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const outlateRouter = express.Router()
+const { signup,signin } = require("../controller/outletController")
+var bodyParser = require("body-parser");
+
+//signup one function
+outlateRouter.use(bodyParser.urlencoded({ extended: false }));
+outlateRouter.use(bodyParser.json());
+
+outlateRouter.post("/signup", signup);
+outlateRouter.post("/signin", signin);
+module.exports = outlateRouter
