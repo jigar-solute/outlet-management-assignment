0cfbd70ddd2aa3d48e514b05337de252b1296b76
VishalP-SoluteLabs <vishal.p@solutelabs.com>
2023-04-19 13:09:04 +0530

    added refresh token logic

--- controller/auth.js
+++ controller/auth.js
@@ -139,10 +139,9 @@ exports.login = async (req, res, next) => {
     const refreshToken = jwt.sign({ email: user.email }, refreshTokenSecret, {
       expiresIn: '1d',
     });
-   if(!user.refreshTokens.includes(refreshToken)){
-    user.refreshTokens.push(refreshToken);
+    
+       user.refreshTokens.push(refreshToken);
     await user.save();
-   }
    
 
     res.status(200).json({
